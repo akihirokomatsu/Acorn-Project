215cbfd282da21685492ef917826868d4b440038
Akihiro Komatsu <33734902+akihirokomatsu@users.noreply.github.com>
2018-01-23 21:30:20 -0500

    Update main.py
    attempting to automate and make the code more flexible, avoiding the use of Excel, and using pandas dataframes

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import csv
-#from django.template.defaultfilters import slugify
+import re
+import pandas as pd
+import os
+import datetime as dt
+
+p = re.compile('^(\s+)(\d{2}/\d{2})(\s+)(\d{2}/\d{2})(\s+)(.{40})(\s+)(\d+)(\s+)(\d+)(\s+)([0-9.]+)$')
+
+my_Files = ['eStmt_2017-01-22.txt', 'eStmt_2017-02-22.txt','eStmt_2017-03-22.txt', 'eStmt_2017-04-22.txt',
+            'eStmt_2017-05-22.txt', 'eStmt_2017-06-22.txt', 'eStmt_2017-07-22.txt', 'eStmt_2017-08-22.txt', 
+            'eStmt_2017-09-22.txt', 'eStmt_2017-10-22.txt', 'eStmt_2017-11-22.txt', 'eStmt_2017-12-22.txt' ]
 
 
-rawfilename = 'cc_transactions.txt'
-processedfilename = 'cc_transactions_parsed.txt'
+# Change working directory
+os.chdir("/Users/akihirokomatsu/Documents/BoA_Statements")
+#initialize list
+results = []
 
-#results = []
+for file in my_Files:
+    with open(file, 'r') as f:
+        for line in f:
+            m = p.match(line)
+            if m:
+                new_line = [m.group(2), m.group(6), m.group(12)]
+                results.append(new_line)
+                print (results)
 
-with open(processedfilename, 'w') as myfile:
-  with open(rawfilename) as f:
-    for line in f:
-      date_split = line.split(' ', 1)[0]
-      r_split_lines = line.rsplit(' ', 1)[1]
-      new_line = date_split + ' ' + r_split_lines
-    
-      #mid_split = line.split() --try to slugify the middle portion 
-      #line = date_split + slugify(line[1:-1]) + r_split_lines
-      #print (slugify(line[12:-7]))
-      
-      myfile.write(new_line)
+#set up dataframe
+#list_of_labels = ['Transaction_Date', 'Description', 'Amount']
+#df = pd.DataFrame(data= results,columns= list_of_labels)
+
+#print (df)
+            
+            
