f482f506d41c4cefd3ac2eede4850f7963c22875
Akihiro Komatsu <33734902+akihirokomatsu@users.noreply.github.com>
2018-01-28 21:44:58 -0500

    Update acorn.py

--- acorn.py
+++ acorn.py
@@ -8,8 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # regular expressions that matches the exact patterns in each bank statement
-p = re.compile('^(\s+)(\d{2}/\d{2})(\s+)(\d{2}/\d{2})(\s+)(.{40})(\s+)(\d+)(\s+)(\d+)(\s+)([0-9.]+)$')
-q = re.compile('^(\d{2}/\d{2})(\s+)(\d{2}/\d{2})(\s+)(.{40})(\s+)(\d+)(\s+)(\d+)(\s+)([0-9.]+)$')
+p = re.compile('^(\s*)(\d{2}/\d{2})(\s+)(\d{2}/\d{2})(\s+)(.{40})(\s+)(\d+)(\s+)(\d+)(\s+)([0-9.]+)$')
 
 my_Files = ['eStmt_2017-01-22.txt', 'eStmt_2017-02-22.txt','eStmt_2017-03-22.txt', 'eStmt_2017-04-22.txt',
             'eStmt_2017-05-22.txt', 'eStmt_2017-06-22.txt', 'eStmt_2017-07-22.txt', 'eStmt_2017-08-22.txt', 
@@ -24,16 +23,11 @@ for file in my_Files:
     with io.open(file, 'r', encoding='latin-1') as f:
         for line in f:
             m1 = p.match(line)
-            m2 = q.match(line)
             if m1:
                 # group(2)= transaction date; group(6)= transaction description; group(12)= transaction amount 
                 new_line1 = [m1.group(2), m1.group(6), m1.group(12)]
                 results.append(new_line1)
-            elif m2:
-                # group(1)= transaction date; group(5)= transaction description; group(11)= transaction amount
-                new_line2 = [m2.group(1), m2.group(5), m2.group(11)]
-                results.append(new_line2)           
-            
+        
 # set up dataframe
 headers = ['Trans_Dt', 'Description', 'Amt']
 df = pd.DataFrame(results)
